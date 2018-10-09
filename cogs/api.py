f107d75f2b9b56eb6ccde73a50f69bfbc876849b
Renaud G <renaud@rolisteam.org>
2018-10-09 12:34:59 +0200

    add api to discordbots.org

---
+++ cogs/api.py
@@ -0,0 +1,35 @@
+import dbl
+import discord
+from discord.ext import commands
+
+import aiohttp
+import asyncio
+import logging
+
+
+class DiscordBotsOrgAPI:
+    """Handles interactions with the discordbots.org API"""
+
+    def __init__(self, bot):
+        self.bot = bot
+        self.token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjI3OTcyMjM2OTI2MDQ1Mzg4OCIsImJvdCI6dHJ1ZSwiaWF0IjoxNTMyMzg4NjU2fQ.qFDHEUhZbe3WMWHc-Uj3wWUIupm192AdJbWcC3IXC44'  #  set this to your DBL token
+        self.dblpy = dbl.Client(self.bot, self.token)
+        self.bot.loop.create_task(self.update_stats())
+
+    async def update_stats(self):
+        """This function runs every 30 minutes to automatically update your server count"""
+
+        while True:
+            logger.info('attempting to post server count')
+            try:
+                await self.dblpy.post_server_count()
+                logger.info('posted server count ({})'.format(len(self.bot.guilds)))
+            except Exception as e:
+                logger.exception('Failed to post server count\n{}: {}'.format(type(e).__name__, e))
+            await asyncio.sleep(1800)
+
+
+def setup(bot):
+    global logger
+    logger = logging.getLogger('bot')
+    bot.add_cog(DiscordBotsOrgAPI(bot))
