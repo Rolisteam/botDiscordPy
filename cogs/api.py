ffe344e1c06e417343c9475bb3c629c3c923aa90
Renaud G <renaud@rolisteam.org>
2018-10-26 10:22:29 +0200

    add log

--- cogs/api.py
+++ cogs/api.py
@@ -10,8 +10,10 @@ import logging
 class DiscordBotsOrgAPI:
     """Handles interactions with the discordbots.org API"""
 
-    def __init__(self, bot):
+    def __init__(self, bot, shardId, shardCount):
         self.bot = bot
+        self.shardId = shardId
+        self.shardCount = shardCount
         self.token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjI3OTcyMjM2OTI2MDQ1Mzg4OCIsImJvdCI6dHJ1ZSwiaWF0IjoxNTMyMzg4NjU2fQ.qFDHEUhZbe3WMWHc-Uj3wWUIupm192AdJbWcC3IXC44'  #  set this to your DBL token
         self.dblpy = dbl.Client(self.bot, self.token)
         self.bot.loop.create_task(self.update_stats())
@@ -22,14 +24,15 @@ class DiscordBotsOrgAPI:
         while True:
             logger.info('attempting to post server count')
             try:
-                await self.dblpy.post_server_count()
-                logger.info('posted server count ({})'.format(len(self.bot.guilds)))
+                await self.dblpy.post_server_count(self.shardCount, self.shardId)
+                logger.info('posted server count ({})'.format(self.dblpy.guild_count()))
+                logger.info('shardCount:{} shardId: {}'.format(self.shardCount,self.shardId))
             except Exception as e:
                 logger.exception('Failed to post server count\n{}: {}'.format(type(e).__name__, e))
             await asyncio.sleep(1800)
 
 
-def setup(bot):
+def setup(bot, shardId, shardCount):
     global logger
     logger = logging.getLogger('bot')
-    bot.add_cog(DiscordBotsOrgAPI(bot))
+    bot.add_cog(DiscordBotsOrgAPI(bot, shardId, shardCount))
